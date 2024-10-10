20c6da68b35cfd7ad8f4081c43ce99235f33970e
Mynchro <webermarius1995@gmail.com>
2024-10-10 15:17:33 +0200

    password-validatoin, login route

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -2,11 +2,37 @@ import bcrypt from "bcrypt";
 import User from "../models/User.js";
 import { connectToDB, closeDB } from "../libs/db.js";
 
-//Registrierung des Users
+//user-registration
 
 export const register = async (req, res) => {
-  const { userName, email, password } = req.body;
+  const { userName, email, password, confirmPassword } = req.body;
+
+  // check if password and confirmPassword match
+
+  if (password !== confirmPassword) {
+    return res
+      .status(400)
+      .json({ message: "Die Passwörter stimmen nicht überein!" });
+  }
+
+  // check password-safety with regex
+
+  const isPasswordStrong = (password) => {
+    const passwordRequirements =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/; // Mindestens 8 Zeichen, mindestens 1 Kleinbuchstabe, 1 Großbuchstabe, 1 Zahl und 1 Sonderzeichen
+    return passwordRequirements.test(password);
+  };
+
+  if (!isPasswordStrong(password)) {
+    return res.status(400).json({
+      message:
+        "Das Passwort muss mindestens 8 Zeichen lang sein und mindestens einen Großbuchstaben, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen enthalten.",
+    });
+  }
+
   try {
+    // connect to db
+
     await connectToDB();
 
     // check existing User
@@ -39,6 +65,43 @@ export const register = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Serverfehler, versuche es erneut" });
+  } finally {
+    await closeDB();
+  }
+};
+
+// user-login
+
+export const login = async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    await connectToDB();
+
+    // check if user exists in the db
+    const existingUser = await User.findOne({ userName });
+    if (!existingUser) {
+      return res.status(400).json({ message: "User existiert nicht!" });
+      // user exists? proceed
+    }
+    // compare password with hash via bcrypt
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Benutzername oder Passwort sind ungültig!" });
+    }
+
+    return res.status(200).json({ message: "Login erfolgreich!" });
+  } catch (error) {
+    console.error("Fehler beim Login:", error);
+    return res
+      .status(500)
+      .json({ message: "Etwas ist schiefgelaufen, probier es nochmal." });
   } finally {
     await closeDB();
   }
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/user.js";
+import { register, login } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/register", register);
-// router.post("login", login);
+router.post("/login", login);
 
 export default router;
