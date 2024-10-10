52b92d3acd8b7aeb9128eba941004851d45f7bf7
Dennis <mayer.dennis90@gmx.de>
2024-10-10 11:23:54 +0200

    first commit

--- Backend/libs/db.js
+++ Backend/libs/db.js
@@ -1,32 +1,35 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const adminKey = encodeURIComponent(process.env.ADMIN);
 const uri = `mongodb+srv://Admin:${adminKey}@nebulaodysseycluster.4gaxp.mongodb.net/?retryWrites=true&w=majority&appName=NebulaOdysseyCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+    tls: true,
+    tlsAllowInvalidCertificates: false,
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connectDB() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        throw error;
+    }
 }
-run().catch(console.dir);
+
+export { connectDB, client };
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { connectDB } from "./libs/db.js";
+//import { Planet } from "./models/Planet.js";
+//import { Building } from "./models/Buildings.js";
 
 const app = express();
-
 app.use(express.json());
 
 const port = 3000;
 
+//await connectDB();
+
 app.listen(port, () => {
-  console.log(`Server hört auf port ${port}`);
+    console.log(`Server is running on port ${port}`);
+
+    //calculateResources();
 });
